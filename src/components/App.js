03185a11a3c003035c5ac467c360f3614f1f23db
Terence1991 <cplterencehawes@gmail.com>
2019-02-02 16:46:31 +0000

    song fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import SongList from './SongList.js'
 
 const App = () => {
 return (
   <div>
-    <songList />
+    <SongList />
   </div>
 )
 }
